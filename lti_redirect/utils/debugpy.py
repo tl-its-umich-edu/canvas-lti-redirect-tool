44317426e740f43e3778a7159b9db8d92ecadd68
Pushyami Gundala <pushyami@umich.edu>
2024-06-20 12:35:42 -0400

    removing the strtobool function

--- lti_redirect/utils/debugpy.py
+++ lti_redirect/utils/debugpy.py
@@ -1,15 +1,11 @@
 from decouple import config
-from distutils.util import strtobool
 import debugpy
 
-
-def forcebool(bool_str):
-    if isinstance(bool_str, str):
-        return strtobool(bool_str)
-    return bool_str
+def config_to_bool(value): 
+    return str(value).lower() in ('true', '1', 'yes', 'on')
 
 def check_and_enable_debugpy():
-    debugpy_enable = forcebool(config('DEBUGPY_ENABLE', default=False))
+    debugpy_enable = config_to_bool(config('DEBUGPY_ENABLE', default=False))
     debugpy_address =  '0.0.0.0'
     debugpy_port = 6020
 
