e47c7c690763cadbdca02c9ba745f0a16c3a5f09
Pushyami Gundala <pushyami@umich.edu>
2024-06-24 09:10:54 -0400

    #15 redirecting to maizey endpoint

--- lti_redirect/views.py
+++ lti_redirect/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect, render
 from lti_tool.views import LtiLaunchBaseView
 from django.contrib.auth.models import User
 from lti_redirect.maizey import SendToMaizey
+from django.http import HttpResponseRedirect
 
 logger = logging.getLogger(__name__)
 
@@ -70,16 +71,16 @@ class ApplicationLaunchView(LtiLaunchBaseView):
             return redirect("error")
         if not login_user_from_lti(request, launch_data):
             return redirect("error")
-        maizey_url= SendToMaizey(launch_data).send_to_maizey()
-        logger.info(f"Maizey URL: {maizey_url}")
-        # logger.info(f"Course JWT: {course_jwt}")
+        maizey_url = SendToMaizey(launch_data).send_to_maizey()
+        if not maizey_url:
+            return redirect("error")
         context = {
             "maizey_url": maizey_url,
         }
-        # if not SendToMaizey(launch_data).send_to_maizey():
-        #     return redirect("error")
-        # return redirect("home")
-        return render(request, "home.html", context)
+        if request.user.is_superuser:
+            return render(request, "home.html", context)
+        else:
+            return HttpResponseRedirect(maizey_url)
 
     def handle_deep_linking_launch(self, request, lti_launch):
         ...  # Optional.
